c0a19b0678606462fb43460e64a2f38af30339b8
Luis Felipe Frutuoso Barni <lff.barni@gmail.com>
2025-03-10 21:34:25 -0300

    dor

---
+++ trabalhos_pascal/cavalo.pas
@@ -0,0 +1,7 @@
+program cavalo;
+var quantidadeCavalos : Integer;
+begin
+    WriteLn('Informe a quantidade de cavalos');
+    ReadLn(quantidadeCavalos);
+    WriteLn('A quantidade de ferraduras necessárias é de: ', quantidadeCavalos*4);
+end.
---
+++ trabalhos_pascal/terreno.pas
@@ -0,0 +1,9 @@
+program terreno;
+
+var x, y : Real;
+
+begin
+    WriteLn('Informe a medida dos lados do terreno:');
+    ReadLn(x, y);
+    WriteLn('A área do terreno é de ', x*y, ' metros quadrados');
+end.
